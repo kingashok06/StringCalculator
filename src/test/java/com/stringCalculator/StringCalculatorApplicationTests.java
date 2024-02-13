22f6a9dada544620b825584de84ec0978466e758
Ashok Kumar <53051357+kingashok06@users.noreply.github.com>
2024-02-13 22:12:56 +0530

    initial commit with base code

---
+++ src/test/java/com/stringCalculator/StringCalculatorApplicationTests.java
@@ -0,0 +1,12 @@
+package com.stringCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Fail.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class StringCalculatorApplicationTests {
+
+}
